ffe7d8d1169544e85ff581be0aad800f198a309e
Nils Putnins <nils@influent.solutions>
2022-02-26 10:35:22 +0200

    Ievades bāze

--- ievades.py
+++ ievades.py
@@ -1,3 +1,15 @@
-produkts = input("Ievadi produkta nosaukumu: ")
-daudzums = input("Ievadi pieejamo daudzumu: ")
+def izvelne():
+    print("Ievadi 1: Pievienot produktu.")
+    print("Ievadi 2: Iziet.")
+    return input("Darbība: ")
 
+izvele = izvelne()
+
+while True:
+    if izvele == '1':
+        produkts = input('Produkta nosaukums: ')
+        daudzums = int(input('Pieejamais daudzums: '))
+        izvele = izvelne()
+
+    elif izvele == '2':
+        break
